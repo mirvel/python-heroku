f884003f6ef52b96f82ee725290087c6a92a26a7
Mirlan Davletov <mirvel@gmail.com>
2020-02-07 18:42:04 -0500

    temp

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,25 +1,13 @@
 from django.urls import path, include
 
 from django.contrib import admin
+from rest_framework import routers
 
-from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets, routers
+from hello.models import UserViewSet
 
 admin.autodiscover()
 import hello.views
 
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 
 # Routers provide an easy way of automatically determining the URL conf.
--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
